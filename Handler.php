f4454bdf2fe77d4bb7a38fbbd93ae113a7d657cd
weilong <wilonx@163.com>
2017-08-11 16:31:36 +0800

    php >= 5.3.9

---
+++ Handler.php
@@ -0,0 +1,159 @@
+<?php
+
+/*
+ * This file is modified from laravel file ——
+ * https://github.com/laravel/framework/blob/5.4/src/Illuminate/Foundation/Bootstrap/HandleExceptions.php
+ *
+ * (c) Weilong Wang <wilonx@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Wilon\Exception;
+
+use Exception;
+use ErrorException;
+use FatalThrowableError;
+use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\Debug\Exception\FatalErrorException;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Application as ConsoleApplication;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Psr\Log\LoggerInterface;
+
+class Handler
+{
+    /**
+     * The Monolog\Logger object.
+     *
+     * @var  \Monolog\Logger
+     */
+    public $logger;
+
+    /**
+     * Bootstrap the HandleExceptions.
+     *
+     * @param  int $level The new error_reporting level.
+     * @param  string $environment
+     * @return void
+     */
+    public function bootstrap($level = -1, $environment = '')
+    {
+        error_reporting($level);
+
+        set_error_handler([$this, 'handleError']);
+
+        set_exception_handler([$this, 'handleException']);
+
+        register_shutdown_function([$this, 'handleShutdown']);
+
+        if ($environment !== 'testing') {
+            ini_set('display_errors', 'Off');
+        }
+
+        if (! $this->logger instanceof LoggerInterface) {
+            $this->setLogger();
+        }
+    }
+
+    /**
+     * Set up the default \Monolog\Logger
+     *
+     * @param  string  $loggerName
+     * @param  string  $loggerFile
+     * @return void
+     *
+     * @throws \ErrorException
+     */
+    public function setLogger($loggerName = 'wilon-exceptions', $loggerFile = 'exceptions.log')
+    {
+        $logger = new Logger($loggerName);
+        $logger->pushHandler(new StreamHandler($loggerFile, Logger::WARNING));
+        $this->logger = $logger;
+        return $this;
+    }
+
+    /**
+     * Convert a PHP error to an ErrorException.
+     *
+     * @param  int  $level
+     * @param  string  $message
+     * @param  string  $file
+     * @param  int  $line
+     * @param  array  $context
+     * @return void
+     *
+     * @throws \ErrorException
+     */
+    public function handleError($level, $message, $file = '', $line = 0, $context = [])
+    {
+        if (error_reporting() & $level) {
+            throw new ErrorException($message, 0, $level, $file, $line);
+        }
+    }
+
+    /**
+     * Handle an uncaught exception.
+     *
+     * Note: Most exceptions can be handled via the try / catch block in
+     * the HTTP and Console kernels. But, fatal error exceptions must
+     * be handled differently since they are not normal exceptions.
+     *
+     * @param  \Throwable  $e
+     * @return void
+     */
+    public function handleException($e)
+    {
+        if (! $e instanceof Exception) {
+            $e = new FatalThrowableError($e);
+        }
+
+        $this->logger->error($e);
+
+        if (php_sapi_name() == 'cli') {
+            (new ConsoleApplication)->renderException($e, new ConsoleOutput);
+        } else {
+            $exception = new ExceptionHandler;
+            $exception->handle($e);
+        }
+    }
+
+    /**
+     * Handle the PHP shutdown event.
+     *
+     * @return void
+     */
+    public function handleShutdown()
+    {
+        if (! is_null($error = error_get_last()) && $this->isFatal($error['type'])) {
+            $this->handleException($this->fatalExceptionFromError($error, 0));
+        }
+    }
+
+    /**
+     * Create a new fatal exception instance from an error array.
+     *
+     * @param  array  $error
+     * @param  int|null  $traceOffset
+     * @return \Symfony\Component\Debug\Exception\FatalErrorException
+     */
+    protected function fatalExceptionFromError(array $error, $traceOffset = null)
+    {
+        return new FatalErrorException(
+            $error['message'], $error['type'], 0, $error['file'], $error['line'], $traceOffset
+        );
+    }
+
+    /**
+     * Determine if the error type is fatal.
+     *
+     * @param  int  $type
+     * @return bool
+     */
+    protected function isFatal($type)
+    {
+        return in_array($type, [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE]);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# exception
+# wilon/exception
+
+Better exception output & write log. Like Laravel.
+
+## Installation & loading
+
+```sh
+composer require wilon/exception
+```
+
+## A Simple Example
+
+```php
+<?php
+
+$a = 123;
+$b = '23333';
+
+simple_dump($a, $b, $_GET, $_SERVER['REMOTE_ADDR']);
+
+simple_log(dirname(__FILE__) . '/somefile.log', $b, $_GET, $_SERVER['REMOTE_ADDR']);
+
+```
+## What's better then var_dump on browser?
+
+```php
+<?php
+
+$longStr = 'President Trump’s ouster of national security adviser Michael T. Flynn, and the circumstances leading up to it, have quickly become a major crisis for the fledgling administration, forcing the White House on the defensive and precipitating the first significant breach in relations between Trump and an increasingly restive Republican Congress.';
+$htmlStr = '<link rel="search" type="application/opensearchdescription+xml" href="/search.osd?v=1483361432" title="Packagist" /><br>';
+$array = pathinfo('C:/test/wilon/index.php');
+
+var_dump($longStr, $htmlStr, $array);
+
+simple_dump($longStr, $htmlStr, $array);
+
+```
+
+![image](https://cloud.githubusercontent.com/assets/7512755/22959116/94ff96a8-f36e-11e6-835e-65d9ebc527cf.png)
+
